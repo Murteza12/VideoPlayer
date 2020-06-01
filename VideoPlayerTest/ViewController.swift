612139c42018f55c89ceec37ad8f5f42974fbb9d
Murteza12 <murteza.raja444@gmail.com>
2020-06-01 21:55:42 +0500

    Initial Commit

---
+++ VideoPlayerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerTest
+//
+//  Created by Murteza on 01/06/2020.
+//  Copyright © 2020 Murteza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
